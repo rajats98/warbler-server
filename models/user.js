9e8b64c47bfccfe8c58e3e9be7a1e3afbdaaed2a
Rajat <rajats1.kv2srg@gmail.com>
2020-01-25 01:25:18 +0530

    store password hash

--- models/user.js
+++ models/user.js
@@ -19,8 +19,31 @@ const userSchema = new mongoose.Schema({
 	profileImageUrl:{
 		type:String
 	}
+})
 
+userSchema.pre("save",async function(next){
+	try {
+		if(!this.isModified("password")){
+			return next();
+		}
+		let hashedPassword = await bcrypt.hash(this.password, 10)  //data,salt 
+		this.password = hashedPassword;
+		return next();
+	} 
+	catch(err){
+		return next(err);
+	}
 });
+
+userSchema.method.comparePassword = async function(candidatePassword, next){
+	try{
+		let isMatch = await bcrypt.compare(candidatePassword,this.password);
+		return isMatch;
+	}
+	catch(err){
+		return next(err);
+	}
+}
 
 const User = model("User",userSchema);
 
