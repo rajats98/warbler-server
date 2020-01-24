38689af814b705d59af03f0cc95d27022b21cdb4
Rajat <rajats1.kv2srg@gmail.com>
2020-01-25 01:05:07 +0530

    basic User model

---
+++ models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema({
+	email:{
+		type:String,
+		required:true,
+		unique: true
+	},
+	username: {
+		type: String,
+		required:true,
+		unique:true
+	},
+	password:{
+		type:String,
+		required:true
+	},
+	profileImageUrl:{
+		type:String
+	}
+
+});
+
+const User = model("User",userSchema);
+
+module.exports = User;
