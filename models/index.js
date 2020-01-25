6c0f3cee3f45209e0a2c06a3cb90436a8cca07f8
Rajat <rajats1.kv2srg@gmail.com>
2020-01-25 17:41:37 +0530

    corrections

--- models/index.js
+++ models/index.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
-mongoose.set("debug":true);
+mongoose.set('debug',true);
 mongoose.Promise = Promise;
 mongoose.connect("mongodb://localhost/warbler",{
 	keepAlive:true,
-	useMongoClient: true
 });
+
+module.exports.User = require("./user")
